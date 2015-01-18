5f5da3870dfc70357678479b6b911e51d5b5a36e
Zack Kourouma <zkourouma@gmail.com>
2015-01-18 13:46:34 -0500

    adds requirements, updates git ignore for vim, and fleshes out url and html xpath

--- sep.py
+++ sep.py
@@ -1,8 +1,19 @@
 import requests
 from lxml import etree
 
-url = "FILL_ME_IN"
+url = "http://plato.stanford.edu/contents.html"
 res = requests.get(url)
 parser = etree.HTMLParser()
 root = etree.fromstring(res.content, parser)
 import ipdb;ipdb.set_trace()
+subject_elements = root.xpath('/html/body/div/div/ul/li')
+topics = []
+for el in subject_elements:
+    title = el.xpath('a').text
+    contrib = el.text
+    topics.append({
+        'title': title.text,
+        'contrib': contrib
+    })
+
+print topics
